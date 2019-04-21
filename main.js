be5e55c1ce83dba2eff1a1989a133b91150a8782
parveenmittal1 <49730713+parveenmittal1@users.noreply.github.com>
2019-04-21 16:45:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ app.post('/concatarray',(req,res) =>{
 app.get('/test11', (req, res) => {
 var url ='https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001815/Alaskan-Malamute-On-White-03-400x267.jpg';
 
-  res.send(<img src='url'>);
+  res.send('<img src='url'>');
 })
 
 
